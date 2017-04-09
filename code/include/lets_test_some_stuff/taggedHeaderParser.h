37bbe0b88950c639a33aae1644c05cde5a386dc3
samGNSS <samuel.schmidt.ee@gmail.com>
2017-04-09 17:07:27 -0400

    Actually commiting the new parser

---
+++ code/include/lets_test_some_stuff/taggedHeaderParser.h
@@ -0,0 +1,53 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_LETS_TEST_SOME_STUFF_TAGGEDHEADER_PARSER_HH
+#define INCLUDED_LETS_TEST_SOME_STUFF_TAGGEDHEADER_PARSER_HH
+
+#include <lets_test_some_stuff/api.h>
+#include <gnuradio/tagged_stream_block.h>
+
+namespace gr {
+  namespace lets_test_some_stuff {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup lets_test_some_stuff
+     *
+     */
+    class LETS_TEST_SOME_STUFF_API taggedHeaderParser : virtual public gr::tagged_stream_block
+    {
+     public:
+      typedef boost::shared_ptr<taggedHeaderParser> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of lets_test_some_stuff::taggedHeaderParser.
+       *
+       * Appends a header to a stream of data
+       */
+      static sptr make();
+    };
+
+  } // namespace lets_test_some_stuff
+} // namespace gr
+
+#endif /* INCLUDED_LETS_TEST_SOME_STUFF_TAGGEDHEADER_H */
+
---
+++ code/lib/taggedHeaderParser_impl.cc
@@ -0,0 +1,113 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "taggedHeaderParser_impl.h"
+
+#define headerSize 2
+#define debug 1
+
+namespace gr {
+  namespace lets_test_some_stuff {
+
+    taggedHeaderParser::sptr
+    taggedHeaderParser::make()
+    {
+      return gnuradio::get_initial_sptr
+        (new taggedHeaderParser_impl());
+    }
+
+    taggedHeaderParser_impl::taggedHeaderParser_impl()
+      : gr::tagged_stream_block("taggedHeaderParser",
+              gr::io_signature::make(1, 1, sizeof(char)),
+              gr::io_signature::make(1, 1, sizeof(char)), "packet_len"),d_prevPacketNumber(0)
+	      {}
+
+    taggedHeaderParser_impl::~taggedHeaderParser_impl(){}
+
+    int
+    taggedHeaderParser_impl::calculate_output_stream_length(const gr_vector_int &ninput_items)
+    {
+      return ninput_items[0] - headerSize; //subtract two bytes for the header
+    }
+
+    int
+    taggedHeaderParser_impl::work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+      const char *in = (const char *) input_items[0];
+      char *out = (char *) output_items[0];
+      uint8_t header[] = {0,0};
+      
+      //parse header
+      std::memcpy(header,in,headerSize);
+      
+      //check for dropped packets
+      if(d_prevPacketNumber++ != header[0]){
+	printf("WARNING: Packet Loss Detected\nExpected packet number: %d got %d\n",d_prevPacketNumber,header[0]);
+	d_prevPacketNumber = header[0];
+      }
+      
+      //wrap packet counter
+      d_prevPacketNumber %= 256;
+      
+      switch(header[1]){
+	case 0:{
+	  //got a pad buffer, drop it on the floor
+	  return 0;
+	}
+	case 1:{
+	  //data packet
+	  break;
+	}
+	default:{
+	  printf("Bad Packet\n");
+	  return 0;
+	}
+      }
+      
+      //fill output buffer
+       std::memcpy(out,in+headerSize,ninput_items[0]-headerSize); //fill the packet after the header
+
+      /*
+      Rearange the tags
+      */
+      std::vector <tag_t> tags;
+      get_tags_in_range(tags, 0, nitems_read(0), nitems_read(0) + ninput_items[0]);
+      for (size_t i = 0; i < tags.size(); ++i){
+        tags[i].offset -= nitems_read(0);
+	if (tags[i].offset > (unsigned int) (ninput_items[0])) {
+          tags[i].offset = ninput_items[0] - 1;
+	}
+        add_item_tag(0, nitems_written(0) + tags[i].offset,tags[i].key,tags[i].value);
+      }
+      
+      // Tell runtime system how many output items we produced.
+      return ninput_items[0] - headerSize;
+    }
+
+  } /* namespace lets_test_some_stuff */
+} /* namespace gr */
